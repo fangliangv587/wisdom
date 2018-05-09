e5c9b22e90bd22667d5694bf691ed4a346356e75
XZ\Administrator <xinxianzhong414@126.com>
2018-05-09 08:46:03 +0800

    增加电源唤醒机制

--- app/src/main/java/com/xz/cenco/weed/TimerService.java
+++ app/src/main/java/com/xz/cenco/weed/TimerService.java
@@ -1,9 +1,11 @@
 package com.xz.cenco.weed;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.support.annotation.Nullable;
 
 import com.cenco.lib.common.DateUtil;
@@ -24,6 +26,7 @@ public class TimerService extends Service implements TimerHelper.TimerListener{
     private WisdomHelper wisdomHelper;
     private ThumberHelper thumberHelper;
     private CoohuaHelper coohuaHelper;
+    private PowerManager.WakeLock mWakeLock;
 
     private MyBinder binder = new MyBinder();
 
@@ -34,6 +37,11 @@ public class TimerService extends Service implements TimerHelper.TimerListener{
         super.onCreate();
 
         LogUtils.w("计时器服务开启");
+
+        //防止休眠
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        mWakeLock = pm.newWakeLock(PowerManager. PARTIAL_WAKE_LOCK  ,"TimerService");
+        mWakeLock.acquire();
 
         //智者提示
         wisdomHelper = new WisdomHelper(this);
@@ -49,7 +57,7 @@ public class TimerService extends Service implements TimerHelper.TimerListener{
 
 
         timerHelper = new TimerHelper(this);
-        timerHelper.setInterval(10);
+        timerHelper.setInterval(5);
         timerHelper.start();
         timerHelper.addListener(wisdomHelper);
         timerHelper.addListener(thumberHelper);
@@ -66,6 +74,11 @@ public class TimerService extends Service implements TimerHelper.TimerListener{
     @Override
     public void onDestroy() {
         super.onDestroy();
+
+        if (mWakeLock!=null){
+            mWakeLock.release();
+        }
+
         if (timerHelper!=null){
             timerHelper.stop();
         }
