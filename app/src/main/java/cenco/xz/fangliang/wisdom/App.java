bad410820612cc3d3a7da32b5c816a5e5df85dcd
XZ\Administrator <xinxianzhong414@126.com>
2018-11-21 17:33:00 +0800

    b

--- app/src/main/java/cenco/xz/fangliang/wisdom/App.java
+++ app/src/main/java/cenco/xz/fangliang/wisdom/App.java
@@ -43,7 +43,7 @@ public class App extends Application {
 
     private void initCommonLib() {
         int level = Level.DEBUG;
-        LogUtils.init("a", level, C.file.log_path);
+        LogUtils.init("b", level, C.file.log_path);
         HttpUtil.init(this);
     }
 
